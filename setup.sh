e6e09852d155b5737ef2d2b2e3eeaf40633eb0b4
Kowser <kowsercse@gmail.com>
2015-02-03 17:51:38 -0600

    Initial setup script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+sudo apt-get install git mysql-server php5 php5-mysql phpmyadmin
+sudo a2enmod rewrite ssl
+
+curl -sS https://getcomposer.org/installer | sudo php -- --filename=composer --install-dir=/usr/bin
